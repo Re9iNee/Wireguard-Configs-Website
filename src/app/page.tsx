c42d93d97894f049b500f8f5e3a079ada9559aab
Re9iNee <attarzadeh76@gmail.com>
2023-08-25 23:20:26 +0330

    Adding Next JS Base

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      Seyed
+    </main>
+  );
+}
