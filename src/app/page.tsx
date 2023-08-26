329b4e680b61a5f756e259c70b0d9aef1a94f2eb
Re9iNee <attarzadeh76@gmail.com>
2023-08-26 13:54:09 +0330

    Completed form and form handler and generating new config files content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,70 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { readFileSync } from "fs";
-import path from "path";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
+import { generateConfigFiles } from "@/lib/utils";
+import Link from "next/link";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
 
 export default function Home() {
-  const serverStatus = readFileSync(
-    path.join("./public/mocks/serverStatus.txt"),
-    "utf8"
-  );
+  // TODO: useTransition?
+  const [file, setFile] = useState<string>("");
+  const [serverStatus, setServerStatus] = useState<string>("");
+  const { toast } = useToast();
+
+  const onFileChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const file = event.target.files?.[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.readAsText(file, "UTF-8");
+    reader.onload = (evt) => {
+      const fileContent = evt.target?.result;
+
+      if (!fileContent) {
+        toast({ title: "Empty Config File" });
+        return;
+      }
+
+      setFile(fileContent.toString());
+    };
+    reader.onerror = () => {
+      toast({
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with reading your file.",
+        variant: "destructive",
+      });
+    };
+  };
+
+  const onServerStatusChange: ChangeEventHandler<HTMLTextAreaElement> = (
+    event
+  ) => {
+    setServerStatus(event.target.value);
+  };
+
+  const formSubmitHandler: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    generateConfigFiles({ inputConfig: file, serverStatus });
+  };
 
   return (
     <main className='flex flex-col items-center justify-between p-24  min-h-screen'>
-      <form action='' className='text-center grid gap-2'>
+      <form className='text-center grid gap-5' onSubmit={formSubmitHandler}>
         <div>
           <Label htmlFor='config_file'>Config File</Label>
-          <Input type='file' id='config_file' />
+          <Input
+            type='file'
+            id='config_file'
+            accept='.conf'
+            onChange={onFileChange}
+          />
         </div>
 
         <div>
@@ -24,13 +72,27 @@ export default function Home() {
           <Textarea
             className='placeholder:grayscale placeholder:opacity-40'
             id='server_status'
-            placeholder={serverStatus}
+            onChange={onServerStatusChange}
+            placeholder={"Paste your server status text"}
             rows={10}
           />
+          {/* TODO: Paste button */}
+          <p className='mt-2 text-xs text-muted-foreground'>
+            You can obtain by sending your config file to this{" "}
+            <Link
+              target='_blank'
+              className='underline hover:text-black'
+              href='https://t.me/WireToolsBot'
+            >
+              Telegram Bot
+            </Link>
+          </p>
         </div>
 
         <Button>Export Config Files</Button>
       </form>
+
+      <Toaster />
     </main>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+type generateConfigFiles = {
+  serverStatus: string;
+  inputConfig: string;
+};
+export async function generateConfigFiles({
+  serverStatus,
+  inputConfig,
+}: generateConfigFiles) {
+  // selects "S08" in "[ S08 ] ( 🟠 ) [ PSD ]"
+  const serverNameRegex = /S\d+\w+/gi;
+  const availableServers = serverStatus.matchAll(serverNameRegex);
+
+  availableServers.next().value;
+
+  for (let sv of availableServers) {
+    // Replace S08 with S8
+    const newSV = sv.toString().replace(/S0(\d)/i, "S$1");
+
+    const newConfigText = inputConfig.replace(/= s\d+/i, `= ${newSV}`);
+    // TODO: await Deno.writeTextFile(`./output/${sv}.conf`, newConfigText);
+    console.dir({ sv, newConfigText });
+  }
 }
