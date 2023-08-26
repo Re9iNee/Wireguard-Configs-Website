245f67bb08a64d074f0c431d00de6d86655c9a84
Re9iNee <attarzadeh76@gmail.com>
2023-08-26 06:57:50 +0330

    adding shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Hello World
+    <main className='items-center justify-between p-24 flex min-h-screen flex-col'>
+      <form action=''>
+        <div className=''>
+          <Label htmlFor='config_file'>Config File</Label>
+          <Input type='file' id='config_file' />
+        </div>
+
+        <div className=''>
+          <Label htmlFor='server_status'>Server Status</Label>
+          <Textarea id='server_status' />
+        </div>
+  <Button>Export Config Files</Button>
+        
+      </form>
     </main>
   );
 }
