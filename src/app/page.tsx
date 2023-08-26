e9e7bfca813d06753449eef7db9c6287ac443fb9
Re9iNee <attarzadeh76@gmail.com>
2023-08-26 07:45:40 +0330

    adding form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,34 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { readFileSync } from "fs";
+import path from "path";
 
 export default function Home() {
+  const serverStatus = readFileSync(
+    path.join("./public/mocks/serverStatus.txt"),
+    "utf8"
+  );
+
   return (
-    <main className='items-center justify-between p-24 flex min-h-screen flex-col'>
-      <form action=''>
-        <div className=''>
+    <main className='flex flex-col items-center justify-between p-24  min-h-screen'>
+      <form action='' className='text-center grid gap-2'>
+        <div>
           <Label htmlFor='config_file'>Config File</Label>
           <Input type='file' id='config_file' />
         </div>
 
-        <div className=''>
+        <div>
           <Label htmlFor='server_status'>Server Status</Label>
-          <Textarea id='server_status' />
+          <Textarea
+            className='placeholder:grayscale placeholder:opacity-40'
+            id='server_status'
+            placeholder={serverStatus}
+            rows={10}
+          />
         </div>
-  <Button>Export Config Files</Button>
-        
+
+        <Button>Export Config Files</Button>
       </form>
     </main>
   );
